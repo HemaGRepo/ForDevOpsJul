698ec141fcac0658104d0593f6f249d562da8ce3
Hema G <hema.g@capgemini.com>
2018-07-13 12:42:29 +0530

    V1.0

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.cg.lnd.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		System.out.println( "Welcome to Maven....!" );
+    }
+}
